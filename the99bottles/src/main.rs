b245dde6b5ffea449b8b28b169c6b764c0c77c7e
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2019-01-29 22:08:21 +0100

    done the 99bottles ^^

--- the99bottles/src/main.rs
+++ the99bottles/src/main.rs
@@ -1,7 +1,23 @@
 
 fn bottles() {
-	println!("in bottle function")
+	let mut bootles_var: i8 = 99;
+	println!("in bottle function");
+	println!("nombre de bouteill {:?}", bootles_var);
+	// bootles_var -= 1;
 
+	loop {
+		println!("{:?} bottles of beer on the wall, {:?} bottles of beer.", bootles_var, bootles_var);
+		bootles_var -= 1;
+		if bootles_var > 0 {
+			println!("Take one down and pass it around, {:?} bottles of beer on the wall.", bootles_var);
+		}
+		else {
+			println!("Take one down and pass it around, no more bottles of beer on the wall.");
+			break;
+		}
+	}
+	println!("No more bottles of beer on the wall, no more bottles of beer.");
+	println!("Go to the store and buy some more, 99 bottles of beer on the wall.");
 }
 
 fn main() {
